c1967be10788a30cba39ba2e0accd8443b2dc32c
Santicabral0478 <scsantiagocabral@gmail.com>
2024-02-06 07:55:32 -0300

    cambio n1

--- front/scripts/tempData.js
+++ front/scripts/tempData.js
@@ -1,6 +1,6 @@
 const tempData = [
   {
-    title: "Guardians of the Galaxy Vol. 2",
+    titulo: "Guardians of the Galaxy Vol. 2",
     year: 2017,
     director: "James Gunn",
     duration: "2h 16min",
@@ -10,7 +10,7 @@ const tempData = [
       "https://m.media-amazon.com/images/M/MV5BNjM0NTc0NzItM2FlYS00YzEwLWE0YmUtNTA2ZWIzODc2OTgxXkEyXkFqcGdeQXVyNTgwNzIyNzg@._V1_SX300.jpg",
   },
   {
-    title: "Star Wars: Episode IV - A New Hope",
+    titulo: "estrella Wars: Episode IV - A New Hope",
     year: 1977,
     director: "George Lucas",
     duration: "2h 1min",
@@ -20,7 +20,7 @@ const tempData = [
       "https://m.media-amazon.com/images/M/MV5BOTA5NjhiOTAtZWM0ZC00MWNhLThiMzEtZDFkOTk2OTU1ZDJkXkEyXkFqcGdeQXVyMTA4NDI1NTQx._V1_SX300.jpg",
   },
   {
-    title: "The Lord of the Rings: The Fellowship of the Ring",
+    titulo: "The Lord of the Rings: The Fellowship of the Ring",
     year: 2001,
     director: "Peter Jackson",
     duration: "2h 58min",
