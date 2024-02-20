a6a2d7c5d4350eadd8869166152e5b3068ba6da1
Santicabral0478 <scsantiagocabral@gmail.com>
2024-02-20 20:53:53 -0300

    creacion de formulario-Peliculas html & css

--- back/src/config/dbCon.js
+++ back/src/config/dbCon.js
@@ -6,7 +6,7 @@ const dbCon = async () => {
         console.log("Conexión a la base de datos establecida correctamente");
     } catch (error) {
         console.error("Error al conectar a la base de datos:", error);
-        throw error; // Lanza el error para que sea manejado en el código que llama a dbCon
+        throw error;
     }
 };
 
--- back/src/models/Movie.js
+++ back/src/models/Movie.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 
 const MovieSchema = new mongoose.Schema({
-    title: String,
-    year: Number,
-    director: String,
-    duration: String,
+    title: {type: String, required: true },
+    year: {type: Number, required: true, min: [1895, "No hay peliculas previas"]},
+    director: {type: String, required: true},
+    duration: {type: String, required: true},
     genre: [String],
-    rate: Number,
-    poster: String
+    rate: {type: Number, required: true},
+    poster: {type: String, required: true}
 });
 
 const Movie = mongoose.model("Movie", MovieSchema);
